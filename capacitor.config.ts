c545718ebdb2f2b8d9af5ff61c60cfcf4c42e81b
Ravindra Saran <ravindrasaran@gmail.com>
2026-03-24 12:47:36 +0530

    Zoran Android setup complete with dist folder

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.education.zoran',
   appName: 'Zoran',
-  webDir: 'build'
+  webDir: 'dist'
 };
 
 export default config;
