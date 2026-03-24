54c84ff1e6256b4d9d0bd36bf1375b827ccdef07
Ravindra Saran <ravindrasaran@gmail.com>
2026-03-24 12:38:03 +0530

    Updated package name to com.education.zoran

---
+++ android/app/src/main/java/com/education/zoran/MainActivity.java
@@ -0,0 +1,5 @@
+package com.education.zoran;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.education.zoran',
+  appName: 'Zoran',
+  webDir: 'build'
+};
+
+export default config;
